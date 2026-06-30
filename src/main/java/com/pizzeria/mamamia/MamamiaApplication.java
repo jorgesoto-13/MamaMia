5e997638bf77299ff1230701b426344474969007
LuiJorge-29 <dragonexpert32@gmail.com>
2026-06-29 22:17:25 -0500

    chore: Setup inicial de Spring Boot y application.properties. Closes #1

---
+++ src/main/java/com/pizzeria/mamamia/MamamiaApplication.java
@@ -0,0 +1,13 @@
+package com.pizzeria.mamamia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamamiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamamiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pizzeria/mamamia/MamamiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pizzeria.mamamia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamamiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
